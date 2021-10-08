2b8f3736f948f494f8d3ef5b79ec600d6fa62406
limonultra <mayuri.furude@gmail.com>
2021-10-08 18:48:51 +0200

    Creado el proyecto react

---
+++ Código Teórico/ParámetrosdeFunción.js
@@ -0,0 +1,22 @@
+import React from "react"
+export const person = (props) => { // podemos en lugar de importar las cosas especificamente
+                            // una a una con {} como en el resto de funciones de este proyecto,
+                            //indicar simplemente que la función tiene argumentos como aquí con props y luego especificar 
+                            //dentro del código con un .age o .whatever 
+    return (
+        <p>
+        props.image;
+        props.age;
+        </p>
+    )};
+
+export const persona = (props) => { // podemos en lugar de importar las cosas especificamente
+        // una a una con {} como en el resto de funciones de este proyecto,
+        //indicar simplemente que la función tiene argumentos como aquí con props y luego especificar 
+        //dentro del código con un .age o .whatever 
+    return (
+        <p>
+        props.image;
+        props.age;
+        </p>
+    )};
