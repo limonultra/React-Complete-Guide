fefe0cd7aded5e567837bf67d533f1f115242db8
limonultra <mayuri.furude@gmail.com>
2021-10-09 18:24:16 +0200

    Inputs, onChange & CSS

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import Person from './Person/Person.js';
 
 class App extends Component{
 
@@ -14,26 +15,51 @@ class App extends Component{
   }
 
 
-switchNameHandler = () => { //es un método de la clase. Le asignamos una función para que se convierta en un método. 
+switchNameHandler = (newName) => { //es un método de la clase. Le asignamos una función para que se convierta en un método. 
   this.setState ({ //el this.setState solo se usa en estos componentes de tipo class.
                   // Solo cambia el valor de aquello que se sobreescribe.
     persons: [
-      {name: 'Maximilian', age: 28},
+      {name: newName, age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 27}
     ] 
   })
 }
 
+nameChangeHandler = (event) => { //Añadimos la palabra Handler como convención para que se sepa que gestiona un tipo de evento
+  this.setState ({  //pasamos un objeto event que recibe los valores del input mediante el event.target
+    persons: [
+      {name: 'Maximilian', age: 28},
+      {name: event.target.value, age: 29},
+      {name: 'Stephanie', age: 27}
+    ] 
+  })
+}
+
   render () {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+    };
+    
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <button onClick={this.switchNameHandler.bind(this,'Ultimate Manuel')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={()=>this.switchNameHandler('Esto es un nuevo nombre')} 
+          changed={this.nameChangeHandler} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     )
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}> I'm {props.name} and I am {props.age} years old!</p> {/* se puede pasar la referencia de una 
+            función a través de los parámetros del componente. En este caso .click es la referencia a la función switchNameHandler */}
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} defaultValue={props.name} />
         </div>
+
     )
 };
 
